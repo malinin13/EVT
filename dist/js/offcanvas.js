959c51b05491775e357dd971c5d7f6248a54ec19
Eugene Malinin <malinin13@gmail.com>
2015-12-13 09:49:19 +0100

    offcanvas fixes

--- dist/js/offcanvas.js
+++ dist/js/offcanvas.js
@@ -1,11 +1,22 @@
 $(document).ready(function () {
     var offcanvas = $('.offcanvas-menu');
     var offcanvasHandler = $('.offcanvas-handler');
+    var offcanvasContainer = $('.offcanvas-container');
     var body = $('body');
 
-    offcanvasHandler.click(function () {
+    offcanvasHandler.click(function (e) {
+        e.stopPropagation();
+        offCanvasToggler();
+    });
+
+    offcanvasContainer.click(function () {
+        offCanvasToggler();
+    });
+
+    function offCanvasToggler() {
+        offcanvasContainer.toggleClass('offcanvas-open');
         body.toggleClass('offcanvas-open');
         offcanvas.toggleClass('offcanvas-open');
-    });
+    }
 
 });
