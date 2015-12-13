78528f2b65a5664ebc50bc54dcb5802f86041d8e
Eugene Malinin <malinin13@gmail.com>
2015-12-13 14:27:07 +0100

    added js bundle

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,26 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var livereload = require('gulp-livereload');
+var concat = require('gulp-concat');
+var	uglify = require('gulp-uglify');
 var connect = require('gulp-connect');
 
+var jsPath = [
+	'bower_components/jquery/dist/jquery.js',
+	'bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
+	'bower_components/owl.carousel/dist/owl.carousel.js',
+	'bower_components/sticky-kit/jquery.sticky-kit.min.js',
+	'bower_components/masonry/dist/masonry.pkgd.min.js',
+	'dist/js/offcanvas.js',
+	'dist/js/main.js'
+];
+
+gulp.task('js-bundle-deps', function(){
+	return gulp.src(jsPath)
+		.pipe(concat('js-bundle.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js'));
+});
 
 // server connect
 gulp.task('connect', function() {
